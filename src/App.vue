038515cc75eee47b36efdf5141054530decd0c82
RecklessSnips <lloyddonegan@gmail.com>
2025-07-26 15:42:05 -0400

    Check environment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+
+console.log('Environment: ', import.meta.env.MODE)
 </script>
 
 <template>
